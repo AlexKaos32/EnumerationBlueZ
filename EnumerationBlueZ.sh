3993b7f4e662425914b84890c0f5b409fdc4ddff
Alex <59406285+AlexKaos32@users.noreply.github.com>
2022-11-06 22:20:07 -0600

    Update EnumerationBlueZ.sh

--- EnumerationBlueZ.sh
+++ EnumerationBlueZ.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 
+#Use hci ifs to run a scan, output to txt
 hcitool scan >> scantest.txt
 
 
+#Pull all bt_addrs from previous text for further use
 grep : scantest.txt | awk '{print $1}' >> addrs.txt
 
 
+#Attempt to browse discovered devices
 while read addr; do
 
         sdptool browse $addr >> browsed.txt
@@ -16,11 +19,15 @@ while read addr; do
 done < addrs.txt
 
 
+#Take any device that wouldn't connect with sdp, output that addr to txt
 grep Failed browsed.txt | cut -d " " -f 8 | sed 's/.$//' >> failed.txt
 
 
+#Set this var to the number of failures from browsing
 fails=$(wc -l < failed.txt)
 
+
+#If there were browse failures, do basic enum with hcitool
 if ((fails > 0)); then
         while read fail; do
                 hcitool info $fail >> hcinfo.txt
@@ -31,4 +38,5 @@ else
 fi
 
 
+#zip extra txt files and remove them
 zip enum_archive scantest.txt addrs.txt failed.txt && rm -rf scantest.txt addrs.txt failed.txt
